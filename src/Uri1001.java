818c3c27c7f7469e98c5228e1926060fdb8ea0f0
Claudio <cernandes.silva@gmail.com>
2020-08-29 00:27:57 -0300

    adicionado novos arquivos

--- src/Uri1001.java
+++ src/Uri1001.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Uri1001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		int A, B, X;
+
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		X = A + B;
+
+		System.out.println("X = " + X);
+
+		sc.close();
 	}
 
 }
+
+
